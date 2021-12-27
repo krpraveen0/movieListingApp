f0890b9134fc42d9be1ade6ebfbc25da8af36b30
krpraveen0 <pkec151015@gmail.com>
2021-12-27 09:25:06 +0530

    widget added- moviesApp/v2

--- moviesProject/moviesApp/forms.py
+++ moviesProject/moviesApp/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from moviesApp.models import Movie
-
+#date widget
+class DateInput(forms.DateInput):
+    input_type='date'
 #creating a movie form
 class MovieForm(forms.ModelForm):
+    movie_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    release_date = forms.DateField(widget=DateInput(attrs={'class':'form-control'}))
+    actor = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    actress= forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    director_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    rating = forms.FloatField(widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model=Movie
-        fields = '__all__'
+        fields = ['movie_name','release_date','actor','actress','director_name','rating']
--- moviesProject/moviesApp/models.py
+++ moviesProject/moviesApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Movie(models.Model):
     release_date = models.DateField()
     movie_name = models.CharField(max_length=30)
+    director_name = models.CharField(max_length=30)
     actor = models.CharField(max_length=30)
     actress = models.CharField(max_length=30)
     rating = models.IntegerField()
